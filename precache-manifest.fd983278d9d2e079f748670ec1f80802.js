9a5ae80d445ed7df5da0b56ac80e813cad4bfd03
tannguyen <tannguyen24894@gmail.com>
2018-12-16 10:37:06 +0700

    Updates

---
+++ precache-manifest.fd983278d9d2e079f748670ec1f80802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1166f3ecb82036638a5",
+    "url": "/react-mario/static/js/runtime~main.c1166f3e.js"
+  },
+  {
+    "revision": "59e779ce3de29bb0b73c",
+    "url": "/react-mario/static/js/main.59e779ce.chunk.js"
+  },
+  {
+    "revision": "15a0818775a39668c4e9",
+    "url": "/react-mario/static/js/1.15a08187.chunk.js"
+  },
+  {
+    "revision": "59e779ce3de29bb0b73c",
+    "url": "/react-mario/static/css/main.05b16881.chunk.css"
+  },
+  {
+    "revision": "fae4458a9689831843ac8e9309e2909d",
+    "url": "/react-mario/index.html"
+  }
+];
